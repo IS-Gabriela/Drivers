60b09980a41dc0c8996bd88e8b310ddf381e42e5
ISGabriela <gaabrielaa@gmail.com>
2023-11-21 19:25:15 -0700

    dia2 PI

--- server/src/models/Driver.js
+++ server/src/models/Driver.js
@@ -4,9 +4,53 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('Driver', {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [3, 20],
+      },
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        len: [20, 500],
+      },
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isUrl: true,
+      },
+    },
+    nationality: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [3, 30],
+      },
+    },
+    dob : {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+      validate: {
+        isDate: true,
+      },
+    },
+  },
+  {
+    timestamps: false,
+  }
+  );
 };
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,5 +1,8 @@
 const { Router } = require("express");
+const DriverRouter = require("./driver_router");
 
 const router = Router();
+
+router.use('/drivers', DriverRouter);
 
 module.exports = router;
